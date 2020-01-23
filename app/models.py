1f926bdd1d351dabe740514d54e69fc5f52deff6
Smile <1073233431@qq.com>
2020-01-23 13:54:53 +0800

    whc

--- app/models.py
+++ app/models.py
@@ -7,31 +7,32 @@
 
 from datetime import datetime
 
-class stu_ques_view_first(db.Model):
+class ques_table(db.Model):
     # 定义表名
     __tablename__ = 'wrong_ques_table'
     # 定义字段
     # db.Column 表示是一个字段
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.BigInteger, primary_key = True)
     student_id = db.Column(db.Integer, unique = True)
     ques_id = db.Column(db.Integer, unique = True)
     ques_info = db.Column(db.String(100), nullable = False)
     ques_answer = db.Column(db.String(100), nullable = False)
-    creator = db.Column(db.char(50), nullable = False)
+    creator = db.Column(db.String(50), nullable = False)
     create_time = db.Column(db.DateTime, nullable = False)
     last_modify_user = db.Column(db.Integer, unique = True)
     last_modify_time = db.Column(db.DateTime, nullable = False)
     is_del = db.Column(db.Integer, nullable = True)
 
-class stu_ques_view_second(db.Model):
+class ques_review(db.Model):
     # 定义表名
     __tablename__ = 'wrong_ques_review'
     # 定义字段
     # db.Column 表示是一个字段
-    ques_id = db.Column(db.Integer, primary_key = True)
-    whether_right = db.Column(db.Boolean)
+    id = db.Column(db.BigInteger, primary_key = True)
+    ques_id = db.Column(db.Integer, unique = True)
+    whether_right = db.Column(db.String(1))
     do_time = db.Column(db.DateTime, unique = True)
-    creator = db.Column(db.char(50), nullable = False)
+    creator = db.Column(db.String(50), nullable = False)
     create_time = db.Column(db.DateTime, nullable = False)
     last_modify_user = db.Column(db.Integer, unique = True)
     last_modify_time = db.Column(db.DateTime, nullable = False)
