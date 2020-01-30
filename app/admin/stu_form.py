f972923489179e9dcacf2b6326ec78a28185967b
Smile <1073233431@qq.com>
2020-01-30 19:33:07 +0800

    错题题干网页第二次提交

--- app/admin/stu_form.py
+++ app/admin/stu_form.py
@@ -1,3 +1,28 @@
 # -*- coding:utf-8 -*-
 # @Author: quxuanye
 # @Time: 2020/1/21 19:53
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, FileField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+from app.models import Admin, Function_list
+from flask_sqlalchemy import SQLAlchemy
+
+'''
+错题题目提交
+'''
+class stu_ques_add(FlaskForm):
+    ques_info = StringField(
+        validators = [
+            DataRequired("题干不能为空")
+        ],
+        render_kw = {
+            "class": "form-control",
+            "placeholder": "题干内容请符合相应规范！"
+        }
+    )
+    submit = SubmitField(
+        "提交题干内容",
+        render_kw={
+            "class": "btn btn-primary"
+        }
+    )
