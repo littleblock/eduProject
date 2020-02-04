894403e41732717029fca0e7d6c544cc83a56991
Qi Xuanye <480808274@qq.com>
2020-02-04 20:12:20 +0800

    知识图谱部分修改

--- app/admin/knowledge_map_view.py
+++ app/admin/knowledge_map_view.py
@@ -5,7 +5,7 @@
 
 from . import admin
 from flask import render_template, redirect, url_for, flash
-from .ques_form import subject_form, period_form
+from .ques_form import subject_form, period_form, knowledge_level1_form
 from app.models import subject, db, period
 from datetime import datetime
 
@@ -28,7 +28,7 @@ def subject_add():
         db.session.add(subject_data)
         db.session.commit()
         flash("录入成功", "ok")
-    return render_template("admin/subject_add.html", title = "添加学科", form = form)
+    return render_template("admin/knowledge_map/subject_add.html", title = "添加学科", form = form)
 
 
 # 学科列表
@@ -38,13 +38,8 @@ def subject_list(page = None):
     if page is None:
         page = 1
     page_datas = subject.query.filter_by(is_del=0).paginate(page = page, per_page = 5)
-    return render_template("admin/subject_list.html", title = "学科列表", page_data = page_datas)
+    return render_template("admin/knowledge_map/subject_list.html", title = "学科列表", page_data = page_datas)
 
-
-# 学科删除
-@admin.route("/subject/del/<id>", methods = ["GET", "POST"])
-def subject_del(id):
-    return "删除学科"
 
 # 添加学段
 @admin.route("/period/add", methods = ["GET", "POST"])
@@ -63,7 +58,7 @@ def period_add():
         db.session.add(period_data)
         db.session.commit()
         flash("录入成功", "ok")
-    return render_template("admin/period_add.html", title="添加学段", form=form)
+    return render_template("admin/knowledge_map/period_add.html", title="添加学段", form=form)
 
 
 # 学段列表
@@ -73,16 +68,51 @@ def period_list(page = None):
     if page is None:
         page = 1
     page_datas = period.query.filter_by(is_del=0).paginate(page = page, per_page = 5)
-    return render_template("admin/period_list.html", title = "学段列表", page_data = page_datas)
+    return render_template("admin/knowledge_map/period_list.html", title = "学段列表", page_data = page_datas)
 
 
-# 添加基础知识点
-@admin.route("knowledge/add", methods = ["GET", "POST"])
-def knowledge_add():
+# 添加一级知识点
+@admin.route("knowledge_level1/add", methods = ["GET", "POST"])
+def knowledge_level1_add():
+    form = knowledge_level1_form()
+    return render_template("admin/knowledge_map/knowledge_level1_add.html", title = "添加一级知识点", form = form)
+
+
+# 一级知识点列表
+@admin.route("/knowledge_level1/list/<id>", methods = ["GET"])
+def knowledge_level1_list(id):
+    return "知识点列表"
+
+# 删除一级知识点
+@admin.route("/knowledge_level1/del/<id>", methods = ["GET", "POST"])
+def knowledge_level1_del(id):
+    return "删除知识点"
+
+# 添加二级知识点
+@admin.route("knowledge_level2/add", methods = ["GET", "POST"])
+def knowledge_level2_add():
     return "添加基础知识点"
 
 
-# 知识点列表
-@admin.route("/knowledge/list/<id>", methods = ["GET", "POST"])
-def knowledge_list(id):
+# 二级知识点列表
+@admin.route("/knowledge_level2/list/<id>", methods = ["GET"])
+def knowledge_level2_list(id):
+    return "知识点列表"
+
+
+# 删除一级知识点
+@admin.route("/knowledge_level2/del/<id>", methods = ["GET", "POST"])
+def knowledge_level2_del(id):
+    return "删除知识点"
+
+
+# 添加三级知识点
+@admin.route("knowledge_level3/add", methods = ["GET", "POST"])
+def knowledge_level3_add():
+    return "添加基础知识点"
+
+
+# 三级知识点列表
+@admin.route("/knowledge_level3/list/<id>", methods = ["GET"])
+def knowledge_level3_list(id):
     return "知识点列表"
--- app/admin/ques_form.py
+++ app/admin/ques_form.py
@@ -5,6 +5,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, FileField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, EqualTo
+from app.models import period
 
 
 '''
@@ -65,3 +66,46 @@ class period_form(FlaskForm):
             "class": "btn btn-primary"
         }
     )
+
+'''
+一级知识点录入
+1. 选择学段，默认初中
+2. 录入知识点名
+'''
+class knowledge_level1_form(FlaskForm):
+    period_name = SelectField(
+        label = "学段分类",
+        description = "学段分类",
+        validators = [
+            DataRequired("分类不能为空！")
+        ],
+        # 从学段表中取出学段信息
+        choices = [],
+        default = 2,
+        coerce = int,
+        render_kw = {
+            "class": "form-control"
+        }
+    )
+    knowledge_level1_name = StringField(
+        label = "一级知识点名",
+        validators = [
+            DataRequired("不能为空！")
+        ],
+        description = "一级知识点名",
+        render_kw = {
+            "class": "form-control",
+            "placeholder": "请输入知识点名称"
+        }
+    )
+    submit = SubmitField(
+        "提交",
+        render_kw={
+            "class": "btn btn-primary"
+        }
+    )
+
+    # 每次实例化都会从数据库取一次，做到数据库和展示页面数据实时更新
+    def __init__(self, *args, **kwargs):
+        super(knowledge_level1_form, self).__init__(*args, **kwargs)
+        self.period_name.choices = [(v.period_id, v.period_name) for v in period.query.filter_by(is_del=0)]
