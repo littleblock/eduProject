e6d07eac07f3d334e6f2550db0fa26c86ac77b39
Smile <1073233431@qq.com>
2020-02-09 00:00:36 +0800

    menu修改

--- app/admin/teacher_view.py
+++ app/admin/teacher_view.py
@@ -2,18 +2,24 @@
 # @Author: wanghaochen
 # @Time: 2020/2/6 18:44
 import os
+import string
+import random
+
 from . import admin
 from app.models import wrong_ques_table, wrong_ques_review, teacher_info, teacher_evaluation, db
 from datetime import datetime
 from flask import render_template, session, url_for, request, flash, redirect
 from .teacher_info_form import teacher_info_form
 from .teacher_evaluate_form import teacher_evaluate_form
+import base64
+
 
 UPLOAD_FOLDER = r'C:\Users\whc\PycharmProjects\eduProject\app\upload'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 Basic_photo=r'C:\Users\whc\PycharmProjects\eduProject\app\upload\touxiang.jpg'
 grade = ["一年级","二年级","三年级","四年级","五年级","六年级","初一","初二","初三","高一","高二","高三"]
 model = ["代数运算","方程组求解","函数","平面几何","立体几何","解析几何","向量运算","排列组合","概率与统计"]
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -50,6 +56,24 @@ def teacher_evaluate():
         db.session.commit()
         flash("提交成功", "ok")
     return render_template("/admin/Teacher/teacher_evaluation.html", form=form)
+''' 上传图片失败
+def editorData(name):
+    #生成随机字符串，防止图片名字重复
+    ran_str = ''.join(random.sample(string.ascii_letters + string.digits, 16))
+    #获取图片文件 name = upload
+    img = name.read()
+    #定义一个图片存放的位置 存放在static下面
+    path = basedir+"\\static\\upload\\"
+    #图片名称 给图片重命名 为了图片名称的唯一性
+    imgName = ran_str+img.filename
+    #图片path和名称组成图片的保存路径
+    file_path = path+imgName
+    #保存图片
+    img.save(file_path)
+    #这个是图片的访问路径，需返回前端（可有可无）
+    url = '/static/upload/'+imgName
+    return url
+'''
 
 @admin.route("/teacher_info_input", methods=["Get", "POST"])
 def teacher_info_input():
@@ -68,7 +92,7 @@ def teacher_info_input():
             if data["photo"] == '':
                 photo = Basic_photo
             else:
-                photo = data["photo"]
+                photo = data["photo"]       # 此处加保存图片程序
             info = teacher_info(teacher_id=1001,  # 写死先
                                 name=data["name"],
                                 head_photo=photo,
@@ -88,3 +112,5 @@ def teacher_info_input():
             db.session.commit()
             flash("提交成功", "ok")
     return render_template("/admin/Teacher/teacher_info.html", form=form)
+
+
